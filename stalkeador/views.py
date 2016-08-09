0de541859acc9b6bb5aaec6881429877369d73be
henriquefacioli <henrique.facioli@gmail.com>
2016-08-08 22:52:27 -0300

    Add search on disciplines

--- stalkeador/views.py
+++ stalkeador/views.py
@@ -2,24 +2,34 @@ from django.shortcuts import render
 from django.db.models import Q
 from dacParser.models import Student, Discipline
 
+
 # This is the index page (homepage).
 # It returns the empty homepage if theres no search_field in the GET request,
 # or returns the search query (NEEDS IMPLEMENTATION)
 def index(request):
     if request.method == 'GET':
         s_id = request.GET.get('search')
-        try:
-            # To search for everything, i'm using Q objetc
-            # [ https://docs.djangoproject.com/en/1.10/ref/models/querysets/ ]
-            # First, looking for students:
-            query = Q(name__contains=s_id) | Q(ra=s_id)
-            students = Student.objects.filter(query).all()
-            results = {
-                'students': students
-                }
-            return render(request, 'stalkeador/home-searcher.html', results)
-        except:
-            return render(request, 'stalkeador/home-searcher.html')
+        if(s_id):
+            try:
+                # To search for everything, i'm using Q objetc
+                # [ https://docs.djangoproject.com/en/1.10/ref/models/querysets/ ]
+                # First, looking for students:
+                query = Q(name__contains=str(s_id)) | Q(ra=s_id)
+                students = Student.objects.all().filter(query)
+
+                query = (Q(code__contains=str(s_id)) |
+                            Q(name__contains=str(s_id)))
+
+                disciplines = Discipline.objects.all().filter(query)
+                results = {
+                    'students': students,
+                    'disciplines': disciplines
+                    }
+
+                return render(request, 'stalkeador/home-searcher.html', results)
+            except:
+                print("ERRO: Problema ao realizar busca")
+        return render(request, 'stalkeador/home-searcher.html')
 
 
 # This is for a student page (/s/RA)
@@ -35,7 +45,6 @@ def student(request, studentRA):
         return render(request, 'stalkeador/student.html')
 
 
-
 # This is for a Discipline page (/s/CODE)
 # returns a list containing all the disciplines with the code
 def discipline(request, code, year, semester, classes):
@@ -47,8 +56,10 @@ def discipline(request, code, year, semester, classes):
                 semester = semester,
                 classes = classes
             )
-            # How its the most especif query, it renders the page of the discipline
-            return render(request, 'stalkeador/discipline.html', {'discipline':discipline})
+            # How its the most especif query, it renders the page of the
+            # discipline
+            return render(request, 'stalkeador/discipline.html',
+                            {'discipline': discipline})
         elif semester:
             disciplines = Discipline.objects.all().filter(
                 code = code.upper(),
