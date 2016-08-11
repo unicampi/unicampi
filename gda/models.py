1e7c0b9b7de20812e8ce09a99e803d9597fc2b59
henriquefacioli <henrique.facioli@gmail.com>
2016-08-11 00:13:23 -0300

    Added a function to generate token

--- gda/models.py
+++ gda/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from dacParser.models import Student, Class
+from gda.tools.tokenGenerator import generateToken
 
 # Create your models here.
 
-class Tokens(models.Model):
+class Token(models.Model):
     student = models.ForeignKey(Student)
     discipline = models.ForeignKey(Class)
-    token = models.CharField(max_length=100)
-    used = models.BooleanField(default=False)
+    token = models.CharField(max_length = 37, default = generateToken())
+    used = models.BooleanField(default = False)
 
     class Meta:
         unique_together = ["student"]
---
+++ gda/tools/tokenGenerator.py
@@ -0,0 +1,7 @@
+import uuid
+
+
+# This is a very simple funcition that generates an uuid
+# Any good ideia would be greate
+def generateToken():
+    return str(uuid.uuid4())
