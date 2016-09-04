e8b726a277c36badcfdc628b67bdf63a316ab0f1
henriquefacioli <henrique.facioli@gmail.com>
2016-09-04 11:28:41 -0300

    Fixed a bug with institutes with more than 4 letters

--- dacParser/urls.py
+++ dacParser/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.updatePage, name='updatePage'),
     url(r'^institutes', views.updateInstitutes, name='updateInstitutes'),
-    url(r'^disciplines/(?P<institute>[A-Za-z]{2,4})', views.updateDisciplines, name='updateDisciplines'),
+    url(r'^disciplines/(?P<institute>[A-Za-z]{2,})', views.updateDisciplines, name='updateDisciplines'),
 ]
