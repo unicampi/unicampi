5fe305784ffc0d059553435e7a32082697e243bf
henriquefacioli <henrique.facioli@gmail.com>
2016-09-08 14:50:21 -0300

    Fixed names with html characters

--- dacParser/views.py
+++ dacParser/views.py
@@ -1,3 +1,5 @@
+import html
+
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.contrib.auth.decorators import login_required
@@ -49,7 +51,7 @@ def updateDisciplines(request, institute):
     # First parses all the classes in this semester
     try:
         print("Parseando disciplinas de "+institute.upper())
-        courses = generateAllCoursesFrom(institute.upper())
+        courses = generateAllCoursesFrom(institute.upper(), 2016, 2)
         print("Terminou de Parsear disciplinas")
     except:
         print("Erro ao parsear disciplinas")
@@ -75,7 +77,6 @@ def updateDisciplines(request, institute):
                 TeacherModel, created = Teacher.objects.get_or_create(
                     name = 'Sem Professor'
                 )
-            print(course.code)
             # Creates discipline Model
             ClasseModel, created = Class.objects.get_or_create(
                 code = course.code,
@@ -92,7 +93,7 @@ def updateDisciplines(request, institute):
             for student in studentsInClass:
                 StudentModel, created = Student.objects.get_or_create(
                     ra = student.ra,
-                    name = student.name,
+                    name = html.unescape(student.name),
                     school = student.school,
                     course_type = student.course_modality,
                 )
