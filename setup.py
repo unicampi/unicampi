32a584a949d632056249e80b7df1ca66e7f3dc6a
Gabriela Surita <gabsurita@gmail.com>
2016-10-30 12:40:52 -0200

    add setup.py

--- setup.py
+++ setup.py
@@ -1,40 +1,32 @@
 # This is the main testing file which can be run as `python setup.py test`
 
 import sys
-from setuptools import setup
-from setuptools.command.test import test as TestCommand
+from setuptools import setup, find_packages
 
+REQUIREMENTS = [
 
-class Tox(TestCommand):
-    # Run tox as a test runner for all tests
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-    def run_tests(self):
-        # Import here, cause outside the eggs aren't loaded
-        import tox
-        errcode = tox.cmdline(self.test_args)
-        cov.stop()
-        sys.exit(errcode)
+]
+
+TEST_REQUIREMENTS = [
+    'tox',
+    'pytest',
+]
 
 
 setup(
-    name='example-project',
-    version='1.0',
-    description='Insert description here',
-    author='Chris Mark',
-    platforms='any',
-    tests_require=['tox'],
-    cmdclass = {'test': Tox},
-    author_email='chris@robinpowered.com',
-
-    # install_requires=['pyserial==2.7',],
-    # packages=[''],
-    # tests_require=['pytest'],
-    # long_description=long_description,
-    # packages=[''],
-    # include_package_data=True,
-    # test_suite='',
+    name='unicampi',
+    license='GNU Public License v3.0',
+    version='0.1',
+    description='API to fetch Unicamp public data.',
+    long_description=open('README.rst').read(),
+    author='Gabriela Surita',
+    author_email='gabsurita@gmail.com',
+    
+    install_requires=REQUIREMENTS,
+    tests_require=TEST_REQUIREMENTS,
+    
+    packages=find_packages(),
+    include_package_data=True,
+    test_suite='tests',
     )
 
