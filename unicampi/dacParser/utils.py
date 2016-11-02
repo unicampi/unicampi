8413fad31e6e23ee72f4ab97dc8320391f487b47
Gabi Surita <gabsurita@gmail.com>
2016-11-01 23:09:06 -0200

    create content searcher class

---
+++ unicampi/dacParser/utils.py
@@ -0,0 +1,31 @@
+from __future__ import unicode_literals
+
+class ContentFinder(object):
+
+    def __init__(self, data):
+        self.data = data
+        self.splited = [s.strip() for s in data.split('\n') if s.strip()]
+        print self.splited
+
+    def find_by_content(self, pattern, offset=1, count=None, end_pattern=None):
+
+        for piece, i in zip(self.splited, range(len(self.splited))):
+            if pattern in piece:
+                start_idx = i
+                break
+
+        if count:
+            return self.splited[start_idx+offset:start_idx+count]
+
+        elif end_pattern:
+            for piece, i in zip(self.splited[start_idx:],
+                            range(start_idx, len(self.splited))):
+                if end_pattern in piece:
+                    end_idx = i
+                    break
+
+            return self.splited[start_idx+offset:end_idx]
+
+        else:
+            return self.splited[start_idx+offset]
+
