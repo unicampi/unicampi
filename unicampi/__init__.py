af9f296c3fb245a6f26cedb38f62e50349512482
Lucas Katayama <lucaskatayama@gmail.com>
2016-11-04 16:53:01 -0200

    Fixing identation

--- unicampi/__init__.py
+++ unicampi/__init__.py
@@ -12,15 +12,15 @@ def main(global_config, **settings):
     config.scan("unicampi.views")
 
     def add_cors_headers_response_callback(event):
-      def cors_headers(request, response):
-          response.headers.update({
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'POST,GET,DELETE,PUT,OPTIONS',
-          'Access-Control-Allow-Headers': 'Origin, Content-Type, Accept, Authorization',
-          'Access-Control-Allow-Credentials': 'true',
-          'Access-Control-Max-Age': '1728000',
-          })
-      event.request.add_response_callback(cors_headers)
+        def cors_headers(request, response):
+            response.headers.update({
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'POST,GET,DELETE,PUT,OPTIONS',
+                'Access-Control-Allow-Headers': 'Origin, Content-Type, Accept, Authorization',
+                'Access-Control-Allow-Credentials': 'true',
+                'Access-Control-Max-Age': '1728000',
+            })
+        event.request.add_response_callback(cors_headers)
 
     config.add_subscriber(add_cors_headers_response_callback, NewRequest)
     return config.make_wsgi_app()
